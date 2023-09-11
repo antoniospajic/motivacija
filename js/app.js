7ede1593ed092ceb30d42d436643220de7e1edf4
Antonio Spajić <carpediem.hr@hotmail.com>
2023-09-11 23:57:24 +0200

    Motivational app - initial commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/* window.$ = require("./jquery"); */
+
+const quotes = [
+  "Vaša budućnost je stvarno svijetla!",
+  "Nema problema kji nemožete riješiti!",
+  "Svaki dan je nova prilika za uspjeh!",
+  "Nikada nemojte odustati od svojih snova!",
+  "Vi ste izvanredna osoba!",
+];
+
+$(function () {
+  const quote = $("#quote");
+  const generateBtn = $("#generate");
+
+  generateBtn.on({
+    click: () => {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      quote
+        .fadeOut(1500, () => {
+          quote.text(quotes[randomIndex]);
+        })
+        .fadeIn();
+    },
+  });
+});
